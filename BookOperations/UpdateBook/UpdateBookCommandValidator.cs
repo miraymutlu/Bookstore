42d3349405364ab43e5509f7fe08fc2f244032aa
Miray Mutlu <miray.mutlu@ozu.edu.tr>
2023-08-31 19:27:21 +0300

    UpdateBookCommandValidator added.

---
+++ BookOperations/UpdateBook/UpdateBookCommandValidator.cs
@@ -0,0 +1,14 @@
+using FluentValidation;
+
+namespace WebApi.BookOperations.UpdateBook;
+
+public class UpdateBookCommandValidator : AbstractValidator<UpdateBookCommand>
+{
+    public UpdateBookCommandValidator()
+    {
+        RuleFor(command => command.Model.Title).NotEmpty();
+        RuleFor(command => command.Model.PageCount).GreaterThan(0);
+        RuleFor(command => command.Model.PublishDate.Date).NotEmpty().LessThan(DateTime.Now.Date);
+        RuleFor(command => command.Model.GenreId).GreaterThan(0);  
+    }
+}
