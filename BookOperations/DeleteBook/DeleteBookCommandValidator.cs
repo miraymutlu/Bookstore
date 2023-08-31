d7222dc71e55ebe9945b5657dac09845c4c72673
Miray Mutlu <miray.mutlu@ozu.edu.tr>
2023-08-31 13:57:33 +0300

    DeleteCommandValidator.cs added

---
+++ BookOperations/DeleteBook/DeleteBookCommandValidator.cs
@@ -0,0 +1,11 @@
+using FluentValidation;
+
+namespace WebApi.BookOperations.DeleteBook;
+
+public class DeleteBookCommandValidator : AbstractValidator<DeleteBookCommand>
+{
+    public DeleteBookCommandValidator()
+    {
+        RuleFor(command => command.BookId).GreaterThan(0);
+    }
+}
