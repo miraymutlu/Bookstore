82840b4f876d1403747d09fd0ef33b92cd469827
Miray Mutlu <miray.mutlu@ozu.edu.tr>
2023-09-04 15:43:04 +0300

    DataGenerator.cs updated

--- DBOperations/DataGenerator.cs
+++ DBOperations/DataGenerator.cs
@@ -15,25 +15,40 @@ public class DataGenerator
             {
                 return; //Data was already seeded.
             }
+            context.Genres.AddRange(
+                new Genre
+                {
+                    Name = "Personal Growth"
+                },
+                new Genre
+                {
+                    Name = "Science Fiction"
+                },
+                new Genre
+                {
+                    Name = "Fantasy"
+                    
+            });
+            
             context.Books.AddRange(
                 new Book
                 {
                     Title = "Lean Startup",
-                    GenreId = 1, //Personal Growth
+                    GenreId = 1, 
                     PageCount = 200,
                     PublishDate = new DateTime(2001, 06, 12)
                 },
                 new Book
                 {
                     Title = "Herland",
-                    GenreId = 2, //Science Fiction
+                    GenreId = 2, 
                     PageCount = 250,
                     PublishDate = new DateTime(2010, 05, 23)
                 },
                 new Book
                 {
                     Title = "Dune",
-                    GenreId = 2, //Science Fiction
+                    GenreId = 2,
                     PageCount = 540,
                     PublishDate = new DateTime(2001, 12, 21)
                 });
