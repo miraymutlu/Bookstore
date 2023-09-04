027c0227ce96bd133fc544d6676d9685633ce080
Miray Mutlu <miray.mutlu@ozu.edu.tr>
2023-09-04 17:17:50 +0300

    Book.cs updated

--- Entities/Book.cs
+++ Entities/Book.cs
@@ -8,6 +8,8 @@ public class Book
     public int Id { get; set; }
     public string Title { get; set; }
     public int GenreId { get; set; }
+    
+    public Genre Genre { get; set; }
     public int PageCount { get; set; }
     public DateTime PublishDate { get; set; }
     
