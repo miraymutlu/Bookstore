58979127fb47f0b1df9138b15c63128894ebb010
Miray Mutlu <miray.mutlu@ozu.edu.tr>
2023-09-04 17:11:28 +0300

    UpdateGenreCommandValidator.cs updated

--- Application/GenreOperations/Commands/UpdateGenre/UpdateGenreCommandValidator.cs
+++ Application/GenreOperations/Commands/UpdateGenre/UpdateGenreCommandValidator.cs
@@ -6,6 +6,6 @@ public class UpdateGenreCommandValidator : AbstractValidator<UpdateGenreCommand>
 {
     public UpdateGenreCommandValidator()
     {
-        RuleFor(command => command.Model.Name).MinimumLength(4).When(x => x.Model.Name.Trim() != string.Empty);
+        RuleFor(command => command.Model.Name).MinimumLength(4).When(x => x.Model.Name != string.Empty);
     }
 }
